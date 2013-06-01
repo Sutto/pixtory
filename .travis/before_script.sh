1a65b3494f269ebb157db329014c88bf6b96a7ee
Darcy Laycock <sutto@sutto.net>
2013-06-01 10:27:35 +0800

    BEFORE SCRIPT.

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,3 +1,3 @@
 createuser pixtory --no-createrole --superuser
 createdb -U postgres pixtory_test
-psql -U postgres -d postgis_adapter_test -c "CREATE EXTENSION postgis;"
+psql -U postgres -d pixtory_test -c "CREATE EXTENSION postgis;"
