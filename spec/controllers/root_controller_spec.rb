12121aa82d05946357204933051c334a3ee757cc
Darcy Laycock <sutto@sutto.net>
2013-06-09 20:51:07 +0800

    Spec fixes.

--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -4,9 +4,15 @@ describe RootController do
 
   describe 'the index action' do
 
-    it 'should work' do
-      get :index
+    it 'should work as json' do
+      get :index, format: 'json'
       response.should be_successful
+    end
+
+    it 'should work as html' do
+      get :index, format: 'html'
+      response.should be_redirect
+      response.should redirect_to explore_url
     end
 
   end
