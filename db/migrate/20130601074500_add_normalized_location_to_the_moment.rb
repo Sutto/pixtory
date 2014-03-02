0035c7dbbe48189154aa8db712505a186b63f515
Darcy Laycock <sutto@sutto.net>
2014-03-02 20:09:46 +1100

    Enable PostGIS

--- db/migrate/20130601074500_add_normalized_location_to_the_moment.rb
+++ db/migrate/20130601074500_add_normalized_location_to_the_moment.rb
@@ -1,5 +1,6 @@
 class AddNormalizedLocationToTheMoment < ActiveRecord::Migration
   def change
+    enable_extension "postgis"
     change_table :moments do |t|
       t.point :coordinates, geographic: true, null: false
       t.index :coordinates, spatial: true
